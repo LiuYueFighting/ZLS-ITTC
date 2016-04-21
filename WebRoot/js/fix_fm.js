a1319230a7f33dcfccbaeca4d5d7cf7d80c528f0
yilvlinyan <1464733454@qq.com>
2016-04-21 16:25:08 +0800

    no message

--- WebRoot/js/fix_fm.js
+++ WebRoot/js/fix_fm.js
@@ -1181,7 +1181,7 @@ oCanvas.domReady(function () {
 
     $("#FM062").click(function(){
 //        if(confirm("是否将“62#阀门”设置为维修状态？")){
-    	$.messager.confirm('确认','是否将<strong>虹吸滤池</strong>设置为维修状态？', function(r){
+    	$.messager.confirm('确认','是否将<strong>62#阀门</strong>设置为维修状态？', function(r){
     		if(r){
             fix_fm062();
             document.getElementById("scheme").onclick="";
