017d4707db23dcc46cf8e69bfd4e89525369b238
gymoon <gymoon@outlook.cn>
2016-05-04 18:12:07 +0800

    膜车间动画。

--- WebRoot/js/mc.js
+++ WebRoot/js/mc.js
@@ -101,6 +101,164 @@ oCanvas.domReady(function () {
         image: "image/mc_test.png"
     });
     canvas.addChild(fg);
+    var bg = canvas.display.image({
+        x: 390,
+        y: 129,
+        height:1,
+        width:1,
+        image: "image/mc_bg2.png"
+    });
+    canvas.addChild(bg);
+    var jtbg = canvas.display.image({
+        x: 390,
+        y: 100,
+        height:1,
+        width:1,
+        image: "image/mc_bg.png"
+    });
+    canvas.addChild(jtbg);
+        
+//    var SC05=createSC({
+//        parent:canvas,
+//        x:830,y:200,height:45,width:45,trail_flag:0,speed:speed_SC*1.2,color:color_SC
+//    });
+    var SC05 = canvas.display.SC_show_down({
+        x: 805,
+        y: 160,
+        Width: 31,
+        Height: 80,
+        height_now: 0,
+        trail_flag: 0,
+        t: 7540,
+        speed:speed_SC*2,
+        fill:color_SC,
+        full:0,
+        start:0
+    });
+    canvas.addChild(SC05);
+    var SC051 = canvas.display.SC_show_down({
+        x: 717,
+        y: 237,
+        Width: 196,
+        Height: 90,
+        height_now: 0,
+        trail_flag: 0,
+        t: 7540,
+        speed:speed_SC*2,
+        fill:color_SC,
+        full:0,
+        start:0
+    });
+    canvas.addChild(SC051);
+    var SC06=createSC({
+        parent:canvas,
+        x:805,y:467,height:80,width:31,trail_flag:0,speed:speed_SC*2,color:color_SC
+    });
+    var SC061=createSC({
+        parent:canvas,
+        x:717,y:377,height:90,width:186,trail_flag:0,speed:speed_SC*2,color:color_SC
+    });
+    var SC07 = canvas.display.SC_show_fill({
+        x: 511,
+        y: 243,
+        Width: 209,
+        Height: 107,
+        height_now: 107,
+        trail_flag: 1,
+        trail: [
+		{x_t: 0, y_t: 0},
+		{x_t: 0, y_t: 107},
+		{x_t: 148, y_t: 107},
+		{x_t: 148, y_t: 87},
+		{x_t: 209, y_t: 87},
+		{x_t: 209, y_t: 0}
+		],
+        t: 7540,
+        speed:speed_SC*1.5,
+//        a:0.1,
+        fill:"rgba(26,215,255,0)",
+        full:0,
+        start:0
+    });
+    canvas.addChild(SC07);
+    
+    var SC071 = canvas.display.SC_show_fill({
+        x: 511,
+        y: 361,
+        Width: 209,
+        Height: 104,
+        height_now: 107,
+        trail_flag: 1,
+        trail: [
+		{x_t: 0, y_t: 0},
+		{x_t: 150, y_t: 0},
+		{x_t: 150, y_t: 17},
+		{x_t: 209, y_t: 17},
+		{x_t: 209, y_t: 104},
+		{x_t: 0, y_t: 104}
+		],
+        t: 7540,
+        speed:speed_SC*1.5,
+//        a:0.1,
+        fill:"rgba(26,215,255,0)",
+        full:0,
+        start:0
+    });
+    canvas.addChild(SC071);
+    
+    var GD04=createGD({
+        parent:canvas,
+        cells:[
+            {x_cell: 821,y_cell: 162},
+            {x_cell: 821, y_cell: 240}
+        ],
+        GDwidth:width_all,
+        color:color_GD
+    });   
+    var GD05=createGD({
+        parent:canvas,
+        cells:[
+            {x_cell: 821,y_cell: 545},
+            {x_cell: 821, y_cell: 467}
+        ],
+        GDwidth:width_all,
+        color:color_GD
+    });
+    var SC08=createSC({
+        parent:canvas,
+        x:680,y:331,height:45,width:345,trail_flag:0,speed:speed_SC*0.1,color:color_SC
+    });
+    var SC081=createSC({
+        parent:canvas,
+        x:438,y:351,height:10,width:242,trail_flag:0,speed:speed_SC*0.05,color:color_SC
+    });
+    var GD06=createGD({
+        parent:canvas,
+        cells:[
+            {x_cell: 571,y_cell: 355},
+            {x_cell: 1020, y_cell: 355}
+        ],
+        GDwidth:width_all,
+        color:color_GD
+    });
+    var GD061=createGD({
+        parent:canvas,
+        cells:[
+            {x_cell: 571,y_cell: 355},
+            {x_cell: 438, y_cell: 355}
+        ],
+        GDwidth:width_all,
+        color:color_GD
+    });
+    
+    var detail = canvas.display.image({
+        x: 390,
+        y: 100,
+        height:1,
+        width:1,
+        image: "image/mc_in.png"
+    });
+    canvas.addChild(detail);     
 
     var txt1=canvas.display.text({
         x:700,
@@ -141,6 +299,18 @@ oCanvas.domReady(function () {
         SC02.advance();
         SC03.advance();
         SC04.advance();
+        GD04.advance();
+        GD05.advance();
+        SC05.advance();
+        SC051.advance();
+        SC06.advance();
+        SC061.advance();
+        SC07.advance();
+        SC071.advance();
+        GD06.advance();
+        GD061.advance();
+        SC08.advance();
+        SC081.advance();
 
         if(GD00.full==1&&GD01.full==1){
         	SC01.start=1;
@@ -149,6 +319,7 @@ oCanvas.domReady(function () {
 //        	fg.width=1;
 //        	fg.height=1;
 //        	canvas.redraw();
+        	
         }
         if(SC01.full==1){
         	SC03.start=1;
@@ -160,18 +331,59 @@ oCanvas.domReady(function () {
         if(GD03.full==1){
         	SC02.start=1;        	
         	GD03.full=0;
+        	
+        }
+        if(SC02.full==1){
+        	detail.width=650;
+        	detail.height=500;
+        	bg.width=650;
+        	bg.height=450;
+        	jtbg.width=650;
+        	jtbg.height=500;
+        	GD04.paused=1;
+        	GD05.paused=1;
+        	SC05.start=1;
+        	SC06.start=1;       	
+        }
+        if(GD04.full==1){
+        	SC051.start=1;
+        	SC061.start=1;
+//        	SC07.start=1;
+//        	console.log(GD06.state);
+//        	GD06.state=3;
+//        	console.log(GD06.state);
+        	
+        }
+        if(SC051.full==1){       	
+        	SC07.start=1;
+        	SC071.start=1;
+        }
+        if(SC07.full==1){       	
+        	GD06.paused=1;
+        	GD061.paused=1;
+        	SC08.start=1;
+        	SC081.start=1;
+        }
+        if(SC081.full==1){       	
+        	SC08.start=1;
         }
         canvas.redraw();
     }).start();
        SC00.start=1;
        GD00.paused=1;
        GD01.paused=1;
+       
        canvas.timeline.start();
     $("#btn_start").click(function(){
         if(this.value==1){
         	SC00.start=1;
         	GD00.paused=1;
             GD01.paused=1;
+//            detail.width=600;
+//        	detail.height=350;
+//        	jtbg.width=600;
+//        	jtbg.height=350;
+            
             canvas.redraw();
             this.innerHTML="&#xe61f;";
             this.value=0;
